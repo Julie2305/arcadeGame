2cc41959b66bf00f055daa718b9c1a4e8f35c261
Julie Teeuwen <julie.teeuwen@nn.nl>
2019-01-12 13:06:48 +0100

    Added player and first enemy on the screen

--- js/app.js
+++ js/app.js
@@ -1,34 +1,54 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+var Enemy = function () {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+  
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+Enemy.prototype.update = function (dt) {
+
+    
+  this.x = 3 * 101;
+  this.y = 2 * 73;
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function () {
+  this.player = 'images/char-boy.png';
+}
 
+Player.prototype.update = function (dt) {
+  this.x = 2 * 101;
+  this.y = 5 * 75;
+}
 
+Player.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.player), this.x, this.y);
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const allEnemies = [
+  new Enemy,
+  new Enemy,
+]
+
+const player = new Player;
 
 
 
